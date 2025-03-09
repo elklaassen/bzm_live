2db5eb34d787e5e221feaf772e20f11047c8dd72
elklaassen <egbert.l.klaassen@gmail.com>
2025-03-09 09:57:12 +0100

    Set port to integer

--- bzm_live.py
+++ bzm_live.py
@@ -1115,4 +1115,4 @@ if __name__ == "__main__":
     #port = int(8080) #int(os.environ.get('PORT', 8050)) # Default to 8050 if PORT is not set
     #app.run_server(debug=True, port=port)
     #app.run_server(host='0.0.0.0', port=port)
-    app.run_server(debug=False, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
